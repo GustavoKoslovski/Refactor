b67cc32b540f5dda57a7414f2551894d1cdc328f
Gustavo <gustavo.koslovski2001@gmail.com>
2022-10-19 19:17:35 -0300

    refactor

--- refactor.js
+++ refactor.js
@@ -1,27 +1,30 @@
+//Função que retorna um número aleatório//
 function returnRandomNumber() {
   return Math.random();
 }
 
+//Função que multiplica o numéro aleatório por 100//
+function numeroRound(num){
+  return Math.round(num * 100);
+}
+
+//Função que multiplica e faz a soma dos números//
 function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
 
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+  return `Seu número é ${numeroRound(returnRandomNumber()) + num}!`;
 }
 
+//Função que multiplica e a subtração dos números//
 function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+  return `Seu número é ${numeroRound(returnRandomNumber()) - num}!`;
 }
 
+//Função que multiplica e faz a multiplicação dos números//
 function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+  return `Seu número é ${numeroRound(returnRandomNumber()) * num}!`;
 }
 
+//Função que multiplica e faz a divisão dos números//
 function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+  return `Seu número é ${numeroRound(returnRandomNumber()) / num}!`;
 }
